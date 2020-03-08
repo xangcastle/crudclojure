569567c787172b2ba7366a6529ad502ea49f84f5
xangcastle <xangcastle@gmail.com>
2020-03-07 19:59:08 -0600

    lein commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to crudclojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/crudclojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns crudclojure.core-test
+  (:require [clojure.test :refer :all]
+            [crudclojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
